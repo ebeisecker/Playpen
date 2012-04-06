7185e95d55b06791694b648b71aef3e636debd40
Eric Beisecker <eric.beisecker@xamarin.com>
2012-04-06 12:01:28 -0400

    Initial Commit of RedLaser Example

---
+++ RedLaserDemo/RedLaserDemo/RedLaserSDKActivity.cs
@@ -0,0 +1,93 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Ebay.Redlasersdk.Scanner;
+using Android.Util;
+using Com.Ebay.Redlasersdk;
+
+namespace RedLaserDemo
+{
+	[Activity (Label = "RedLaserSDKActivity", MainLauncher = true)]
+	public class RedLaserSDKActivity : BarcodeScanActivity
+	{
+		const string TAG = "RedLaserSDK";
+		
+		public const string DO_UPCE = "do_upce";
+		public const string DO_EAN8 = "do_ean8";
+  		public const string DO_EAN13 = "do_ean13";
+  		public const string DO_STICKY = "do_sticky";
+  		public const string DO_QRCODE = "do_qrcode";
+  		public const string DO_CODE128 = "do_code128";
+  		public const string DO_CODE39 = "do_code39";
+  		public const string DO_CODE93 = "do_code93";
+  		public const string DO_DATAMATRIX = "do_datamatrix";
+  		public const string DO_RSS14 = "do_rss14";
+  		public const string DO_ITF = "do_itf";
+		
+		protected override int BeepResource
+		{
+			get { return R.Raw.Beep; }
+		}
+		
+		protected override int LogoResource
+		{
+			get { return R.Drawable.OverlayLogo; }
+		} 
+		
+		protected override void OnCreate (Bundle bundle)
+		{
+			Log.Debug(TAG, "Creating a new RedLaserSDK object");
+			
+			var settings = new RedLaserSettings();
+			settings.AlignBarcode = "Align barcode inside box";
+			settings.HoldStill = "Hold still for scan";
+			
+			base.OnCreate (bundle);
+			
+			var extras = Intent.Extras;
+			
+			Hints.Upce = extras.GetBoolean(DO_UPCE);
+			Hints.Ean8 = extras.GetBoolean(DO_EAN8);
+			Hints.Ean13 = extras.GetBoolean(DO_EAN13);
+			Hints.QRCode = extras.GetBoolean(DO_QRCODE);
+			Hints.Code128 = extras.GetBoolean(DO_CODE128);
+			Hints.Code39 = extras.GetBoolean(DO_CODE39);
+			Hints.Code93 = extras.GetBoolean(DO_CODE93);
+			Hints.DataMatrix = extras.GetBoolean(DO_DATAMATRIX);
+			Hints.ITF = extras.GetBoolean(DO_ITF);
+			Hints.RSS14 = extras.GetBoolean(DO_RSS14);
+			Hints.Sticky = extras.GetBoolean(DO_STICKY);
+			
+			SetButtons("Button", "Button 2", "Button 3");
+			
+			Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+		}
+		
+		protected override sealed void OnButton1Click()
+		{
+			//throw new NotImplementedException ();
+		}
+		
+		protected override sealed void OnButton2Click()
+		{
+			//throw new NotImplementedException ();
+		}
+		
+		protected override void OnButton3Click ()
+		{
+			//throw new NotImplementedException ();
+		}
+		
+		protected override void OnBarcodeScanned (BarcodeResult barcode)
+		{
+			ReturnResult(barcode);
+		}
+	}
+}
+
+
---
+++ RedLaserDemo/RedLaserDemo/RedLaserSampleActivity.cs
@@ -0,0 +1,208 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace RedLaserDemo
+{
+	[Activity (Label = "RedLaserSampleActivity")]			
+	public class RedLaserSampleActivity : Activity
+	{
+		const string TAG = "RedLaserSampleActivity";
+		
+		bool doUpce = true;
+		bool doEan8 = false;
+		bool doEan13 = false;
+		bool doSticky = false;
+		bool doQRCode = false;
+		bool doCode128 = false;
+		bool doCode39 = false;
+		bool doCode93 = false;
+		bool doDataMatrix = false;
+		bool doRSS14 = false;
+		bool doITF = false;
+		
+		private string udid;
+		  		
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			Title = "RedLaser SDK Demo";
+			
+			SetContentView(2130903040);
+			
+			var btnScan = (ToggleButton)FindViewById(2131165192); // btnScan
+			btnScan.Click += (sender, e) => this.LaunchScanner();
+			
+			var udidView = (TextView)FindViewById(2131165191); // udidString
+			udidView.SetText("UDID: " + this.udid, TextView.BufferType.Normal);
+			
+			var toggleUPC = (ToggleButton)FindViewById(2131165185); // toggleUPC
+			toggleUPC.Checked = this.doUpce;
+			toggleUPC.Click += (sender, e) => 
+			{
+				if(toggleUPC.Checked){
+					doUpce = true;
+					Toast.MakeText(this, "OPC On", 0).Show();
+				}
+				else{
+					doUpce = false;
+					Toast.MakeText(this, "OPC off", 0).Show();
+				}
+			};
+			
+			var toggleEAN = (ToggleButton)FindViewById(2131165186); // Toggle EAN
+			toggleEAN.Checked = doEan8;
+			toggleEAN.Click += (sender, e) => 
+			{
+				if(toggleEAN.Checked){
+					doEan8 = true;
+					doEan13 = true;
+					Toast.MakeText(this, "EAN On", 0).Show();
+				}
+				else{
+					doEan8 = false;
+					doEan13 = false;
+					Toast.MakeText(this, "EAN Off", 0).Show();
+				}
+			};
+			
+			var toggleQR = (ToggleButton)FindViewById(2131165187); // Toggle QR
+			toggleQR.Checked = doQRCode;
+			toggleQR.Click += (sender, e) => 
+			{
+				if(toggleQR.Checked){
+					doQRCode = true;
+					Toast.MakeText(this, "QR Code On", 0).Show();
+				}
+				else {
+					doQRCode = false;
+					Toast.MakeText(this, "QR Code Off", 0).Show();
+				}
+			};
+			
+			var toggle128 = (ToggleButton)FindViewById(2131165188); // toggle128
+			toggle128.Checked = doCode128;
+			toggle128.Click += (sender, e) => 
+			{
+				if(toggle128.Checked){
+					doCode128 = true;
+					Toast.MakeText(this, "Code 128 On", 0).Show();
+				}
+				else{
+					doCode128 = false;
+					Toast.MakeText(this, "Code 128 Off", 0).Show();
+				}
+			};
+			
+			var toggle39 = (ToggleButton)FindViewById(2131165189); // toggle39
+			toggle39.Checked = doCode39;
+			toggle39.Click += (sender, e) => 
+			{
+				if(toggle39.Checked){
+					doCode39 = true;
+					Toast.MakeText(this, "Code 39 On", 0).Show();
+				}
+				else{
+					doCode39 = false;
+					Toast.MakeText(this, "Code 39 Off", 0).Show();
+				}
+			};
+		}
+		
+		protected override void OnResume ()
+		{
+			base.OnResume ();
+			
+			var toggleUPC = (ToggleButton)FindViewById(2131165185); // toggleUPC
+			toggleUPC.Checked = doUpce;
+			
+			var toggleEAN = (ToggleButton)FindViewById(2131165186); // Toggle EAN
+			toggleEAN.Checked = doEan8;
+			
+			var toggleQR = (ToggleButton)FindViewById(2131165187); // Toggle QR
+			toggleQR.Checked = doQRCode;
+			
+			var toggle128 = (ToggleButton)FindViewById(2131165188); // toggle128
+			toggle128.Checked = doCode128;
+			
+			var toggle39 = (ToggleButton)FindViewById(2131165189); // toggle39
+			toggle39.Checked = doCode39;
+		}
+		
+		protected override void OnActivityResult (int requestCode, Result resultCode, Android.Content.Intent data)
+		{
+			if(resultCode == Result.Ok){
+				var barcode = data.Action;
+				var barcodeType = data.GetStringExtra("barcode_type");
+				Log.Debug(TAG, "BARCODE: " + barcode);
+				
+				new AlertDialog.Builder(this).SetTitle("Scan Result").SetMessage(barcodeType + ": " + barcode)
+				.SetNegativeButton("OK", (Dialog, whichButton) => { }).Show();
+			}
+		}
+		
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+		    base.OnCreateOptionsMenu (menu);
+			var inflater = MenuInflater; 
+			inflater.Inflate(2131099648, menu); // main
+			return true;
+		}
+		
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			switch(item.ItemId)
+			{
+			case 2131165195: // scan
+				LaunchScanner();
+				return true;
+			}
+			
+			return base.OnOptionsItemSelected(item);
+		}
+		
+		public void LaunchScanner()
+		{
+			try
+			{
+				var bundle = BuildBundle();
+				var scanIntent = new Intent(this, typeof(RedLaserSDKActivity));
+				scanIntent.PutExtra("Scans", bundle);
+				StartActivityForResult(scanIntent, 1);
+			}
+			catch(Exception e)
+			{
+				Log.Debug(TAG, e.Message + " " + e.StackTrace);
+			}
+		}
+		
+		public Bundle BuildBundle()
+		{
+			Bundle bundle = new Bundle();
+			
+			bundle.PutBoolean(RedLaserSDKActivity.DO_UPCE, doUpce);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_EAN8, doEan8);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_EAN13, doEan13);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_STICKY, doSticky);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_QRCODE, doQRCode);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_CODE128, doCode128);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_CODE39, doCode39);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_CODE93, doCode93);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_DATAMATRIX, doDataMatrix);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_RSS14, doRSS14);
+			bundle.PutBoolean(RedLaserSDKActivity.DO_ITF, doITF);
+			
+			return bundle;
+		}
+	}
+}
+
